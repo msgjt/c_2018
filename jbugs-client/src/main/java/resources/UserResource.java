7853ebae898149660c01b27c1341218346ab24a7
giacob <giurgi.iacob@gmail.com>
2018-08-20 11:37:52 +0300

    Test for master branch

--- jbugs-client/src/main/java/resources/UserResource.java
+++ jbugs-client/src/main/java/resources/UserResource.java
@@ -8,7 +8,6 @@ import javax.ejb.EJB;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -25,20 +24,12 @@ public class UserResource {
                 .entity(new Gson().toJson(userManagement.getAllUsers()))
                 .build();
     }
-
     @POST
     @Produces(MediaType.APPLICATION_JSON)
     public Response getUsersPost() {
-        String[] blabla = {"Gica", "Eu", "Gica", "Eu", "Haam", "Eu",};
+        String[] blabla = {"Gica","Eu","Gica","Eu","Gica","Eu",};
         return Response.status(Response.Status.OK)
                 .entity(new Gson().toJson(blabla))
                 .build();
-    }
-
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    @Path("/{userName}")
-    public Response getUserByUsername(@PathParam("userName") String userName){
-        return Response.status(Response.Status.OK).entity(userManagement.getUserByUsername(userName)).build();
     }
 }
