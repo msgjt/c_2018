ee45cf6aa773cde90173487d54abc1972a37dcec
RusIoan <rus_ioan96@yahoo.com>
2018-08-20 10:05:56 +0200

    improved userResource and user business management

--- jbugs-client/src/main/java/resources/UserResource.java
+++ jbugs-client/src/main/java/resources/UserResource.java
@@ -7,6 +7,7 @@ import ro.msg.edu.jbugs.userManagement.business.control.UserManagement;
 import javax.ejb.EJB;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -22,5 +23,12 @@ public class UserResource {
         return Response.status(Response.Status.OK)
                 .entity(new Gson().toJson(userManagement.getAllUsers()))
                 .build();
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/{userName}")
+    public Response getUserByUsername(@PathParam("userName") String userName){
+        return Response.status(Response.Status.OK).entity(userManagement.getUserByUsername(userName)).build();
     }
 }
