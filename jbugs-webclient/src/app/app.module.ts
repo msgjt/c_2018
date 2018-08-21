2355ef711a61faa1455455024dd55fe7717f4b3e
BaciuFlorin <florin.baciu96@gmail.com>
2018-08-21 15:42:10 +0300

    create user angular prototype

--- jbugs-webclient/src/app/app.module.ts
+++ jbugs-webclient/src/app/app.module.ts
@@ -1,38 +1,45 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 import {LoginModule} from './login/login.module';
 import {HttpClientModule} from '@angular/common/http';
 import {RouterModule, Routes} from '@angular/router';
-import { AuthGuard } from './auth/auth.guard';
+import {AuthGuard} from './auth/auth.guard';
 import {FormsModule} from '@angular/forms';
-import { HeaderComponent } from './header/header.component';
-import { PermissionComponent } from './permission/permission.component';
-import { RoleComponent } from './role/role.component';
+import {HeaderComponent} from './header/header.component';
+import {PermissionComponent} from './permission/permission.component';
+import {RoleComponent} from './role/role.component';
+import {CreateUserComponent} from './create-user/create-user.component';
+import { NgSelectModule } from '@ng-select/ng-select';
 
 
 const appRoutes: Routes = [
   {path: '', pathMatch: 'full', redirectTo: '/login'},
-  {path: 'permission', component:PermissionComponent},
-  {path: 'role', component:RoleComponent},
+  {path: 'permission', component: PermissionComponent},
+  {path: 'role', component: RoleComponent},
+  {path: 'create-user', component: CreateUserComponent},
 
 ]
+
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     PermissionComponent,
-    RoleComponent
+    RoleComponent,
+    CreateUserComponent,
   ],
   imports: [
     BrowserModule,
     LoginModule,
     HttpClientModule,
     FormsModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    NgSelectModule
   ],
   providers: [AuthGuard],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ jbugs-webclient/src/app/create-user/create-user.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import {$} from "jQuery";
+import {Role} from "../services/role.service";
+
+@Component({
+  selector: 'app-create-user',
+  templateUrl: './create-user.component.html',
+  styleUrls: ['./create-user.component.css']
+})
+export class CreateUserComponent implements OnInit {
+  roles: Role[] = [];
+  selectedItems: Role[] = [];
+
+  constructor() { }
+  ngOnInit() {
+  }
+
+  onItemSelect(role: Role){
+    this.selectedItems.push(role);
+  }
+
+}
