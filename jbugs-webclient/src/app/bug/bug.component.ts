49eaf323a37a03216eddda0882d13fd2f14a42c7
kedith <kardaedith98@gmail.com>
2018-08-21 11:02:46 +0300

    created first frontend classes for bug management

---
+++ jbugs-webclient/src/app/bug/bug.component.ts
@@ -0,0 +1,12 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: 'app-bug',
+  templateUrl: './bug.component.html',
+  styleUrls: ['./bug.component.css']
+})
+
+export class BugComponent{
+
+  constructor(){}
+}
