d5f76634aaa10b856ae344e8e08f9503fab88abf
giacob <giurgi.iacob@gmail.com>
2018-08-24 12:20:03 +0300

    Update bug ver.3.0

--- jbugs-webclient/src/app/update-bug/update-bug.component.ts
+++ jbugs-webclient/src/app/update-bug/update-bug.component.ts
@@ -31,6 +31,10 @@ export class UpdateBugComponent implements OnInit {
 
 
   constructor(private bugService: BugService,private userService: UserService) {
+    this.attachmentToBeAdded = {
+      bugDTO:null,
+      blob: ""
+    }
   }
 
   ngOnInit(): void {
@@ -87,15 +91,14 @@ export class UpdateBugComponent implements OnInit {
     })[0];
   }
 
-  fileChange($event) {
-    this.attachmentToBeAdded = null;
+  fileChange() {
     var reader: FileReader = new FileReader();
     let eventTarget = <HTMLInputElement>event.target;
     if (eventTarget.files && eventTarget.files.length > 0) {
       let file = eventTarget.files[0];
+      console.log("file name"+ file.name);
       reader.onload = function () {
         this.attachmentToBeAdded.blob = reader.result;
-        console.log(this.attachmentToBeAdded.blob)
       }.bind(this);
       reader.readAsText(file);
     }
@@ -131,7 +134,7 @@ export class UpdateBugComponent implements OnInit {
       return value.bugDTO.idBug == bug.idBug;
     });
     console.log('Bugul ' + bug.idBug + ' are ' + this.attachmentsForABug.length + ' atasamente');
-    // this.attachmentToBeAdded.bugDTO = bug;
+    this.attachmentToBeAdded.bugDTO = bug;
   }
 
   editableFunction(bug: Bug): boolean {
@@ -160,6 +163,7 @@ export class UpdateBugComponent implements OnInit {
   }
 
   addAttachment(attachmentChosen:Attachment){
+    console.log(attachmentChosen);
     this.bugService.addAttachment(attachmentChosen);
   }
 
