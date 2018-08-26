7274dfa80302a3081a4491d1667f39a8cd9f3108
kedith <kardaedith98@gmail.com>
2018-08-26 16:26:33 +0300

    Commented insert attachments temporarily

--- jbugs-persistence/src/main/resources/META-INF/loadTestdata.sql
+++ jbugs-persistence/src/main/resources/META-INF/loadTestdata.sql
@@ -95,7 +95,7 @@ insert into comments(date, text, idBug, idUser) values (CURRENT_TIMESTAMP , 'som
 insert into users_roles(id_user,id_role) values (1,1);
 insert into users_roles(id_user,id_role) values (2,2);
 
-INSERT INTO attachments(idBug,file) values (1,LOAD_FILE('C:/Users/giurgi/Documents/JBugs/jbugs-persistence/src/main/resources/attachments/blabla.txt'));
+/*INSERT INTO attachments(idBug,file) values (1,LOAD_FILE('C:/Users/giurgi/Documents/JBugs/jbugs-persistence/src/main/resources/attachments/blabla.txt'));
 INSERT INTO attachments(idBug,file) values (1,LOAD_FILE('C:/Users/giurgi/Documents/JBugs/jbugs-persistence/src/main/resources/attachments/blabla.txt'));
 INSERT INTO attachments(idBug,file) values (1,LOAD_FILE('C:/Users/giurgi/Documents/JBugs/jbugs-persistence/src/main/resources/attachments/blabla.txt'));
 INSERT INTO attachments(idBug,file) values (2,LOAD_FILE('C:/Users/giurgi/Documents/JBugs/jbugs-persistence/src/main/resources/attachments/blabla.txt'));
@@ -105,4 +105,6 @@ INSERT INTO attachments(idBug,file) values (2,LOAD_FILE('C:/Users/giurgi/Documen
 INSERT INTO attachments(idBug,file) values (1,LOAD_FILE('C:/Users/giurgi/Documents/JBugs/jbugs-persistence/src/main/resources/attachments/blabla.txt'));
 INSERT INTO attachments(idBug,file) values (1,LOAD_FILE('C:/Users/giurgi/Documents/JBugs/jbugs-persistence/src/main/resources/attachments/blabla.txt'));
 INSERT INTO attachments(idBug,file) values (1,LOAD_FILE('C:/Users/giurgi/Documents/JBugs/jbugs-persistence/src/main/resources/attachments/blabla.txt'));
+*/
+-- ToDo decomment attachments 
 -- other tables TODO
