eef8695795b077c6c132c5e4fd2d2a8d5c7eb3b0
giacob <giurgi.iacob@gmail.com>
2018-08-28 11:58:56 +0300

    Try for attachments

--- jbugs-business/src/main/java/ro/msg/edu/jbugs/userManagement/business/service/utils/ByteToFilesConverter.java
+++ jbugs-business/src/main/java/ro/msg/edu/jbugs/userManagement/business/service/utils/ByteToFilesConverter.java
@@ -1,5 +1,7 @@
 package ro.msg.edu.jbugs.userManagement.business.service.utils;
 
+import org.apache.commons.codec.binary.Base64;
+
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
@@ -12,10 +14,11 @@ import java.nio.file.Paths;
 public class ByteToFilesConverter {
 
     public BufferedImage getImagefromBytes(byte[] bytes){
+        System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaa Converter lungime sir: " + bytes.length);
         BufferedImage img = null;
         try {
             img = ImageIO.read(new ByteArrayInputStream(bytes));
-            File outputFile = new File("C:/Users/giurgi/Desktop/saved.jpg");
+            File outputFile = new File("C:/Users/giurgi/Desktop/saved_photo.jpg");
             ImageIO.write(img,"jpg",outputFile);
             return img;
 
--- jbugs-client/src/main/java/controller/AttachmentController.java
+++ jbugs-client/src/main/java/controller/AttachmentController.java
@@ -4,13 +4,12 @@ import com.google.gson.Gson;
 import ro.msg.edu.jbugs.userManagement.business.dto.bug.AttachmentDTO;
 import ro.msg.edu.jbugs.userManagement.business.service.bug.IBugBusinessService;
 import ro.msg.edu.jbugs.userManagement.business.service.utils.ByteToFilesConverter;
-import ro.msg.edu.jbugs.userManagement.persistence.entity.Attachment;
+import ro.msg.edu.jbugs.userManagement.persistence.entity.ExtensionEnum;
 
 import javax.ejb.EJB;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.io.*;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -19,18 +18,22 @@ public class AttachmentController {
     @EJB
     private IBugBusinessService bugBusinessService;
 
-    byte[] fileBytes = new byte[500000];
+    static byte[] fileBytes = new byte[1000000];
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getAttachments(){
+    public Response getAttachments() {
         List<AttachmentDTO> attachmentDTOS = bugBusinessService.getAllAttachments();
         ByteToFilesConverter byteToFilesConverter = new ByteToFilesConverter();
 
-        AttachmentDTO attachmentDTO = attachmentDTOS.get(attachmentDTOS.size()-1);
-        System.out.println(attachmentDTO.getBlob().toString().getBytes().length);
-//        Fi = attachmentDTO.getBlob();
-//            byteToFilesConverter.getDocfromBytes(array);
+        AttachmentDTO attachmentDTO = attachmentDTOS.get(attachmentDTOS.size() - 1);
+        if (attachmentDTO.getExtension().equals(ExtensionEnum.JPG)) {
+            byteToFilesConverter.getImagefromBytes(attachmentDTO.getBlob());
+        }
+        if (attachmentDTO.getExtension().equals(ExtensionEnum.DOC)) {
+            byteToFilesConverter.getDocfromBytes(attachmentDTO.getBlob());
+        }
+
         return Response.status(Response.Status.OK)
                 .entity(new Gson().toJson(attachmentDTOS))
                 .build();
@@ -40,7 +43,7 @@ public class AttachmentController {
     @POST
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response addAttachment(AttachmentDTO attachmentDTO){
+    public Response addAttachment(AttachmentDTO attachmentDTO) {
         attachmentDTO.setBlob(this.fileBytes);
         return Response.status(Response.Status.OK)
                 .entity(new Gson().toJson(bugBusinessService.addAttachment(attachmentDTO)))
@@ -50,8 +53,10 @@ public class AttachmentController {
     @Path("/file")
     @POST
     @Consumes(MediaType.MULTIPART_FORM_DATA)
-    public Response getFile(byte[] file){
+    public Response getFile(byte[] file) {
         this.fileBytes = file;
+        ByteToFilesConverter byteToFilesConverter = new ByteToFilesConverter();
+        byteToFilesConverter.getImagefromBytes(this.fileBytes);
         return Response.status(Response.Status.OK)
                 .entity(new Gson().toJson("received"))
                 .build();
@@ -61,9 +66,9 @@ public class AttachmentController {
     @Path("/{idBug}")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getAttachments(@PathParam("idBug") long idBug){
+    public Response getAttachments(@PathParam("idBug") long idBug) {
 
-        List<AttachmentDTO> attachmentDTOS = bugBusinessService.getAllAttachments().stream().filter(x ->x.getBugDTO().getIdBug().equals(idBug)).collect(Collectors.toList());
+        List<AttachmentDTO> attachmentDTOS = bugBusinessService.getAllAttachments().stream().filter(x -> x.getBugDTO().getIdBug().equals(idBug)).collect(Collectors.toList());
         return Response.status(Response.Status.OK)
                 .entity(new Gson().toJson(attachmentDTOS))
                 .build();
@@ -73,7 +78,7 @@ public class AttachmentController {
     @POST
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response deleteAttachment(AttachmentDTO attachmentDTO){
+    public Response deleteAttachment(AttachmentDTO attachmentDTO) {
         return Response.status(Response.Status.OK)
                 .entity(new Gson().toJson(bugBusinessService.deleteAttachment(attachmentDTO)))
                 .build();
