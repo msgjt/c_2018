e25e21e51781d363468de0e4bca42fa142a7aad5
BaciuFlorin <florin.baciu96@gmail.com>
2018-08-30 23:13:54 +0300

    export pdf bug after review done

--- jbugs-webclient/src/app/services/export-pdf.service.ts
+++ jbugs-webclient/src/app/services/export-pdf.service.ts
@@ -3,85 +3,57 @@ import * as jsPDF from 'jspdf'
 import {Bug} from "../types/bugs";
 import {Comment, CommentClass} from "..//types/comments";
 import {Attachment} from "../types/attachment";
-import {el} from "@angular/platform-browser/testing/src/browser_util";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ExportPDFService {
+  column = 30;
+  row = 10;
+  doc = new jsPDF();
 
   constructor() {
   }
 
-  // getSplitText(text: string, doc: jsPDF): string{
-  //   var maxLength = doc.internal.pageSize.width - 10;
-  //   var splitText = doc.splitTextToSize(text, maxLength);
-  //   console.log(splitText);
-  //   doc.text(0.5, 0.5, lines)
-  //   return splitText;
-  // }
-
   export(bug: Bug, comments: Comment[], attachments: Attachment[]) {
-    var doc = new jsPDF();
-    var numberComments = 0;
-    let row = 60;
 
-    var lMargin=15; //left margin in mm
-    var rMargin=15; //right margin in mm
-    var pdfInMM=210;  // width of A4 in mm
-    var h=doc.internal.pageSize.height=297;
     let namesAttachments = "";
     if (attachments.length > 0) {
       attachments.forEach(value => namesAttachments = namesAttachments + " " + value.name);
     } else
       namesAttachments = "none";
-    doc.setPage(1);
-
-    doc.text("JBugger raport of bug #" + bug.idBug, 70, 20);
-    doc.text("Title: " + bug.title, 30, 40);
-    doc.text("Revision/Version: " + bug.version, 30, 50);
-    doc.text("Fixed in version/Revision: " + bug.fixedVersion, 30, 60);
-    doc.text("Target date: " + bug.targetDate, 30, 70);
-    doc.text("Severity: " + bug.severity, 30, 80);
-    doc.text("Created by: " + bug.createdByUser.username, 30, 90);
-    doc.text("Status: " + bug.status, 30, 100);
-    doc.text("Assigned to: " + bug.assignedTo.username, 30, 110);
-    doc.text("Attachments: " + namesAttachments, 30, 120);
-    var description = bug.description;
-    var lines = doc.splitTextToSize(description, (pdfInMM-lMargin-rMargin));
-    doc.text("Description: ", 30, 130);
-    doc.text(30,140, lines);
-    doc.addPage();
-    doc.setPage(2);
-    doc.text("Comments", 30, 40); //85.160
+    this.doc.text("JBugger raport of bug #" + bug.idBug, this.column + 40, this.incrementRow(10));
+    this.doc.text("Title: " + bug.title, this.column, this.incrementRow(10));
+    this.doc.text("Revision/Version: " + bug.version, this.column, this.incrementRow(10));
+    this.doc.text("Fixed in version/Revision: " + bug.fixedVersion, this.column, this.incrementRow(20));
+    this.doc.text("Target date: " + bug.targetDate, this.column, this.incrementRow(20));
+    this.doc.text("Severity: " + bug.severity, this.column, this.incrementRow(20));
+    this.doc.text("Created by: " + bug.createdByUser.username, this.column, this.incrementRow(20));
+    this.doc.text("Status: " + bug.status, this.column, this.incrementRow(20));
+    this.doc.text("Assigned to: " + bug.assignedTo.username, this.column, this.incrementRow(20));
+    this.doc.text("Attachments: " + namesAttachments, this.column, this.incrementRow(20));
+    this.doc.text("Description: ", this.column, this.incrementRow(20));
+    this.addTextOnPage(bug.description);
+    this.doc.text("---Comments---", this.column + 55, this.incrementRow(20)); //85.160
     for (let comment of comments) {
-      console.log("nr comm: "+numberComments);
-      doc.text("#" + comment.id, 30, row)
-      doc.text(comment.date , 30, row + 8);
-      var comm = comment.text;
-      var lines2 = doc.splitTextToSize(comm,(pdfInMM-lMargin-rMargin));
-      doc.text(comment.user,30,row+16);
-      doc.text(lines2,30,row+32);
-      row = row + 40;
-
-      // while (numberComments < 5) {
-      //
-      //   console.log("nr comm: ", numberComments);
-      //   doc.text("#" + comment.id, 30, row)
-      //   doc.text(comment.date, 30, row + 8);
-      //   doc.text(comment.user + ": " + comment.text, 30, row + 16);
-      //   row = row + 32;
-      //   numberComments++;
-      // }
-      // doc.addPage();
-      // numberComments = 0;
-      // row = 60;
-      // console.log("nr comm dupa addpage: ", numberComments);
+      this.addTextOnPage("#" + comment.id + " " + comment.date + " " + comment.user + " - " + comment.text);
     }
+    this.doc.save('Bug#' + bug.idBug + '.pdf');
+  }
 
+  incrementRow(value: number) {
+    this.row = this.row + value;
+    return this.row;
+  }
 
-    // Save the PDF
-    doc.save('Bug#' + bug.idBug + '.pdf');
+  addTextOnPage(value: string) {
+    let pageHeight = this.doc.internal.pageSize.height = 257;
+    if (this.row >= pageHeight) {
+      this.doc.addPage();
+      this.row = 0 // Restart height position
+    }
+    var lines = this.doc.splitTextToSize(value, (150));
+    this.doc.text(this.column, this.incrementRow(20), lines);
   }
 
 
