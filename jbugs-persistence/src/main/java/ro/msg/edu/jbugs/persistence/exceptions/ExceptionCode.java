14389325cbcfdee491bb978a4623daf43a21c150
RusIoan <rus_ioan96@yahoo.com>
2018-09-01 19:52:39 +0200

    good night folks

--- jbugs-persistence/src/main/java/ro/msg/edu/jbugs/persistence/exceptions/ExceptionCode.java
+++ jbugs-persistence/src/main/java/ro/msg/edu/jbugs/persistence/exceptions/ExceptionCode.java
@@ -6,7 +6,8 @@ package ro.msg.edu.jbugs.persistence.exceptions;
 public enum ExceptionCode {
     ROLE_NOT_FOUND_EXCEPTION(1001,"Role not found Exception"),
     EMAIL_NOT_FOUND_EXCEPTION(1002, "Email not found Exception"),
-    USERNAME_NOT_FOUND_EXCEPTION(1003, "Username not found Exception");
+    USERNAME_NOT_FOUND_EXCEPTION(1003, "Username not found Exception"),
+    USER_NOT_FOUND_EXCEPTION(1004, "User not found exception");
 
     private int id;
     String message;
